3127f6da6ff52c634acbc0662bc13cc446268197
washingtonprograma@gmail.com <teu@email.com>
2026-01-19 12:59:37 -0300

    First commit

---
+++ src/main/java/com/oxtin/comandBar/ComandBarApplication.java
@@ -0,0 +1,13 @@
+package com.oxtin.comandBar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxtin/comandBar/ComandBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxtin.comandBar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
