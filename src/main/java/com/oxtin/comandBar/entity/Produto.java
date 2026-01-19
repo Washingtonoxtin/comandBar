db874b3b63e1709f1bfd93039bd6530664b2db3f
Washington Oliveira Alves <washingtonprograma@gmail.com>
2026-01-19 19:17:55 -0300

    First Entity

---
+++ src/main/java/com/oxtin/comandBar/entity/Produto.java
@@ -0,0 +1,27 @@
+package com.oxtin.comandBar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+
+}
