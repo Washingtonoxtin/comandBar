da1a319487f1d574ecef28bc20f796df86ea6876
washingtonprograma@gmail.com <teu@email.com>
2026-01-23 15:25:42 -0300

    Ajuste created

--- src/main/java/com/oxtin/comandBar/controller/ProdutoController.java
+++ src/main/java/com/oxtin/comandBar/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.oxtin.comandBar.controller;
 import com.oxtin.comandBar.entity.Produto;
 import com.oxtin.comandBar.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -14,7 +15,8 @@ public class ProdutoController {
 
     private final ProdutoRepository repository;
 
-    @PostMapping("/salva")
+    @PostMapping("/cadastrar")
+    @ResponseStatus(HttpStatus.CREATED)
     public Produto salva(@RequestBody Produto produto) {
         return repository.save(produto);
     }
